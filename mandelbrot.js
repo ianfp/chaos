426bafbf65e447cf1e64444ce4445d3873aa531b
Ian Phillips <ifphillips@gmail.com>
2017-08-19 21:53:21 -0700

    Remove a possible optimization that doesn't work
    This approach actually slows things down by 3-4x

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,15 +1,11 @@
 function isMandelbrot(complex) {
 	const maxIter = 10;
-	// visited = new Set();
 	let current = Complex.ZERO;
 	for (let iter = 0; iter < maxIter; iter++) {
 		current = current.mul(current).add(complex);
 		if (willGoToInfinity(current)) {
 			return false;
 		}
-		// if (visited.has(current.toString())) {
-		// 	return true;
-		// }
 	}
 	return true; // Hasn't run away yet, so assume it won't.
 }
